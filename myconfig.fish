a428dae5c0a58ca2fbb7b1a92e6e230aeda59c44
wyahiro <wyahiro@indeed.com>
2019-01-17 20:54:36 +0900

    fix

--- myconfig.fish
+++ myconfig.fish
@@ -1,7 +1,7 @@
 set -l MYCONFIG_DIR (dirname (status filename))
 
 # Set path to my functions
-set -px fish_function_path $MYCONFIG_DIR/functions
+set -p fish_function_path $MYCONFIG_DIR/functions
 
 # fzf
 set -U FZF_LEGACY_KEYBINDINGS 0
