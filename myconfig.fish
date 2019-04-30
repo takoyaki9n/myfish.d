0b18906dedf9c27ec363ad83177e62ab14f77450
Wataru Yahiro <wataru@mac-book-pro.local>
2019-05-01 05:51:41 +0900

    remove autojump

--- myconfig.fish
+++ myconfig.fish
@@ -6,14 +6,6 @@ set -p fish_function_path $MYCONFIG_DIR/functions
 # fzf
 set -U FZF_LEGACY_KEYBINDINGS 0
 
-# autojump
-begin
-    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
-    if test -e $AUTOJUMP_PATH
-        source $AUTOJUMP_PATH
-    end
-end
-
 # OS specific settings
 switch (uname)
     case Linux # Linux
