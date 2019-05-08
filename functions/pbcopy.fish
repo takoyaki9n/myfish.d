c66225caf243253807491538d828a97a3a3da799
wyahiro <wyahiro@indeed.com>
2019-05-08 00:08:40 -0500

    update pbpaste & pbcopy for linux

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -2,7 +2,7 @@ function pbcopy -d 'Copy to the Clipboard'
     set -l pbcopy (which pbcopy)
     switch (uname)
         case Linux
-            set pbcopy 'xsel --clipboard --input'
+            set pbcopy 'xsel' '--clipboard' '--input'
     end
     $pbcopy $argv
 end
--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -2,7 +2,7 @@ function pbpaste -d 'Paste from the Clipboard'
     set -l pbpaste (which pbpaste)
     switch (uname)
         case Linux
-            set pbpaste 'xsel --clipboard --output'
+            set pbpaste 'xsel' '--clipboard' '--output'
     end
     $pbpaste $argv
 end
